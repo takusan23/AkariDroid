23e292d5f21d3a2caeaba0b40915b887451d927d
takusan23 <takumu1200@gmail.com>
2024-03-18 02:52:28 +0900

    RenderItemDisplayTimeEditComponent で使ってみた

--- app/src/main/java/io/github/takusan23/akaridroid/ui/component/RenderItemDisplayTimeEditComponent.kt
+++ app/src/main/java/io/github/takusan23/akaridroid/ui/component/RenderItemDisplayTimeEditComponent.kt
@@ -1,12 +1,11 @@
 package io.github.takusan23.akaridroid.ui.component
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import io.github.takusan23.akaridroid.RenderData
 
 /** [RenderData.DisplayTime]を編集する共通コンポーネント */
@@ -15,21 +14,28 @@ fun RenderItemDisplayTimeEditComponent(
     displayTime: RenderData.DisplayTime,
     onUpdate: (RenderData.DisplayTime) -> Unit
 ) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(5.dp)
-    ) {
-        OutlinedLongTextField(
-            modifier = Modifier.weight(1f),
-            value = displayTime.startMs,
-            onValueChange = { startMs -> onUpdate(displayTime.copy(startMs = startMs)) },
-            label = { Text(text = "表示開始時間（ミリ秒）") }
-        )
-        OutlinedLongTextField(
-            modifier = Modifier.weight(1f),
-            value = displayTime.durationMs,
-            onValueChange = { durationMs -> onUpdate(displayTime.setDuration(durationMs)) },
-            label = { Text(text = "表示時間（ミリ秒）") }
-        )
+    Column {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = "表示開始時間"
+            )
+            DurationInput(
+                modifier = Modifier.weight(2f),
+                durationMs = displayTime.startMs,
+                onChange = { startMs -> onUpdate(displayTime.copy(startMs = startMs)) }
+            )
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = "表示時間"
+            )
+            DurationInput(
+                modifier = Modifier.weight(2f),
+                durationMs = displayTime.durationMs,
+                onChange = { durationMs -> onUpdate(displayTime.setDuration(durationMs)) }
+            )
+        }
     }
 }
