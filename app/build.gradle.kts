ccd14e8076899c96031f19313cfaf8146deddc5e
takusan23 <takumu1200@gmail.com>
2024-05-30 02:52:49 +0900

    versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "io.github.takusan23.akaridroid"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "2.1.0"
 
         // アプリのビルド時間をアプリ側で取得できるように
