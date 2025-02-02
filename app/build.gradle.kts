c60d7cc23e455c6d08a921b9b043d005cbb3e85f
takusan23 <takumu1200@gmail.com>
2025-02-03 02:06:51 +0900

    4.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.takusan23.akaridroid"
         minSdk = 21
         targetSdk = 35
-        versionCode = 10
-        versionName = "4.1.1"
+        versionCode = 11
+        versionName = "4.1.2"
 
         // アプリのビルド時間をアプリ側で取得できるように
         resValue("string", "build_date", System.currentTimeMillis().toString())
